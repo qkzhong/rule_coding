0893b77b6c8ae34cb503dd567a627b8c7745cd2d
Seth Frey <moctodliamg@gmail.com>
2019-11-24 14:51:27 -0800

    code and comment improvements
    added upper and lower bound to ADICO, and columns to output
    added a dependencies
    a few code suggestiosn (like use of enumerate)
    added comments

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Package needed: numpy, csv, pandas
+Package needed: numpy, csv, pandas, xlrd
 input file: rules_data_codifying.xlsx
 output file: compare.csv
--- compare.py
+++ compare.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import csv
 
+### HELPERS
 #unique transformed in python
 def unique(list):
     x = np.array(list)
@@ -29,6 +30,7 @@ def agreement(answers):
     result.append(answers.count(i))
   return(max(result)/len(answers))
 
+### SCRIPT
 #read document
 xls = pd.ExcelFile('rules_data_codifying.xlsx')
 coders = xls.sheet_names[1:11]
@@ -38,25 +40,31 @@ columns = Master.to_dict('split')['columns']
 #read each sheet
 full_dict = {}
 x = 1
-for coder in coders:
-  sheet = xls.parse(x)
+for x, coder in enumerate(coders):
+  sheet = xls.parse( x+1 )
   full_dict[coder] =  sheet.to_dict('index')
-  x = x + 1
 
 dic_size = len(full_dict['Statements_Arti'])
-fieldnames = ['Text Type', 'Institution Type', 'Rule/Norm/Strategy', 'Level of Analysis', 'Attribute', 'Deontic', 'aIm', 'oBject', 'Or Else', 'Condition']
+fieldnames_in = ['Text Type', 'Institution Type', 'Rule/Norm/Strategy', 'Level of Analysis', 
+                 'Attribute', 'Deontic', 'aIm', 'oBject', 'Or Else', 'Condition']
+fieldnames_out = ['Text Type', 'Institution Type', 'Rule/Norm/Strategy', 'Level of Analysis', 
+                  'Attribute_lo', 'Attribute_hi', 'Deontic_lo', 'Deontic_hi', 
+                  'aIm_lo', 'aIm_hi', 'oBject_lo', 'oBject_hi', 'Or Else_lo', 'Or Else_hi', 
+                  'Condition_lo', 'Condition_hi']
 ABDICO = ['Attribute', 'Deontic', 'aIm', 'oBject', 'Or Else', 'Condition']
+# build output
 with open('compare.csv',mode ='w') as compare_file:
-    compare_writer = csv.DictWriter(compare_file, fieldnames = fieldnames)
+    compare_writer = csv.DictWriter(compare_file, fieldnames = fieldnames_out)
     compare_writer.writeheader()
     for i in range(0,dic_size):
         row = {}
-        for item in fieldnames:
+        for item in fieldnames_in:
             answers = []
             for coder in coders:
                 answers.append(full_dict[coder][i][item])
             if item in ABDICO:
-                row[item]= upper_bound(answers)
+                row[item+'_hi']= upper_bound(answers)
+                row[item+'_lo']= agreement(answers)
             else:
                 row[item]= agreement(answers)
         compare_writer.writerow(row)
